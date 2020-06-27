4bada8080d6aaba193b7a0c2d91383f2dbf182f9
Sean Hodges <sean.hodges@nserc-crsng.gc.ca>
2020-06-27 12:41:51 -0400

    Starting the re-implementation of the Dialogue

---
+++ 4900Project/Assets/Scenes/DialogueScene/DFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scenes.DialogueScene {
+    /// <summary>
+    /// The DFunctions class provides some utility functions that can be used as button handlers.
+    /// This includes things like controlling the dialog & triggering the quest system.
+    /// </summary>
+    static class DFunctions {
+        /// <summary>
+        /// Advances the Dialogue system to the next page.
+        /// </summary>
+        public static readonly Action GoToNextPage = () => {
+
+        };
+
+        /// <summary>
+        /// Closes the Dialogue.
+        /// </summary>
+        public static readonly Action CloseDialogue = () => {
+
+        };
+
+        /// <summary>
+        /// Notifies the quest system that a button was pressed.
+        /// </summary>
+        public static readonly Action NotifyQuestSystem = () => {
+
+        };
+    }
+}
---
+++ 4900Project/Assets/Scenes/DialogueScene/Interfaces/IDButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scenes.DialogueScene.Interfaces {
+    /// <summary>
+    /// The interface for a single dialog button. Every button has text & a click handler.
+    /// </summary>
+    interface IDButton {
+        // The button's text. This is what gets displayed for the button.
+        public string Text { get; set; }
+
+        // The click handler. This will be called when the button gets clicked.
+        public Action OnButtonClick { get; set; }
+    }
+}
