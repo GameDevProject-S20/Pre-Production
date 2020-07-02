71b2ac1c24afea30cd73e22b4bd165ab707d06f3
Keegan <kkoch1397@gmail.com>
2020-07-01 21:27:20 -0400

    QuestManager reimplemented

--- 4900Project/Assets/Scripts/QuestManager.cs
+++ 4900Project/Assets/Scripts/QuestManager.cs
@@ -33,25 +33,82 @@ public class QuestManager : MonoBehaviour
     Dictionary<string, Quest> completedQuests = new Dictionary<string, Quest>();
     Dictionary<string, Quest> allQuests = new Dictionary<string, Quest>();
 
-    /*public TransactionEvent transactionEvent;
+    public EventManager.TransactionEvent transactionEvent;
 
 
-    public DialogueEvent dialogueEvent;
+    public EventManager.DialogueSelectionEvent dialogueEvent;
 
  
-    public QuestEvent questCompleteEvent;
+    public EventManager.QuestEvent QuestCompleteEvent;
 
-    public QuestEvent questObjectiveCompleted;
+    public EventManager.QuestEvent QuestObjectiveCompleted;
 
-    public QuestEvent questActivated;
-    public QuestEvent questProgressed;
-    */
+    public EventManager.QuestEvent QuestActivated;
+    public EventManager.QuestEvent QuestProgressed;
+    
     public void AddQuest(Quest quest)
     {
         string qn = quest.Name;
         inactiveQuests.Add(qn, quest);
         allQuests.Add(qn, quest);
+
+        quest.QuestComplete.AddListener(() =>
+        {
+            activeQuests.Remove(qn);
+            readyQuests.Add(qn, quest);
+            QuestObjectiveCompleted.Invoke(quest);
+        });
+
+        quest.QuestProgressed.AddListener(() =>
+        {
+            QuestProgressed.Invoke(quest);
+        });
+
+        QuestActivated.Invoke(quest);
+
     }
+
+    public void StartQuest(string questName)
+    {
+        if (inactiveQuests.ContainsKey(questName))
+        {
+            activeQuests.Add(questName, inactiveQuests[questName]);
+            inactiveQuests.Remove(questName);
+        }
+    }
+
+    public void CompleteQuest(string questName)
+    {
+        GetQuest(questName, out Quest questData);
+        completedQuests.Add(questName, questData);
+        if(questData.TurnInButton)
+        {
+            readyQuests.Remove(questName);
+        }
+        else
+        {
+            activeQuests.Remove(questName);
+        }
+
+        QuestCompleteEvent.Invoke(questData);
+    }
+
+    public void GetReadyQuests(out IEnumerable<string> quests)
+    {
+        quests = readyQuests.Keys;
+    }
+
+    public bool GetQuest(string questName,out Quest questData)
+    {
+        return allQuests.TryGetValue(questName, out questData);
+    }
+
+    public IEnumerable<Quest> GetQuests()
+    {
+        return allQuests.Values;
+    }
+
+
 
     // Can call this, but shouldn't need to after the journal has been initialized
     public void UpdateJournal()
