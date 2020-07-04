567c6e6ca8d663a34a9360ef632d69228aee51c0
Sean Hodges <sean.hodges@nserc-crsng.gc.ca>
2020-07-04 12:22:19 -0400

    Rebase / Add JSON support

--- 4900Project/Assets/Scripts/Data/Files/FileConstants.cs
+++ 4900Project/Assets/Scripts/Data/Files/FileConstants.cs
@@ -15,7 +15,6 @@ namespace FileConstants
     {
         public string GoogleDriveFileId;
         public string LocalBackupFile;
-        public string MimeType;
     }
 
     /// <summary>
@@ -27,40 +26,42 @@ namespace FileConstants
         public static readonly FileStorageData Dialog = new FileStorageData()
         {
             GoogleDriveFileId = "1_7nd016Df8DQaQcoTO5uVkwkHLHL_RIIVejc0AGOE_U",
-            LocalBackupFile = $"{Application.dataPath}/BackupData/Dialog.csv",
-            MimeType = "text/csv"
+            LocalBackupFile = $"{Application.dataPath}/BackupData/Dialog.csv"
         };
 
         // Items CSV
         public static readonly FileStorageData Items = new FileStorageData()
         {
             GoogleDriveFileId = "1FfBoXktnMt-L44RBr4YOiQFztfTFeJVKOX56EC_FxjE",
-            LocalBackupFile = $"{Application.dataPath}/BackupData/Items.csv",
-            MimeType = "text/csv"
+            LocalBackupFile = $"{Application.dataPath}/BackupData/Items.csv"
         };
 
         // Town CSV
         public static readonly FileStorageData Town = new FileStorageData()
         {
             GoogleDriveFileId = "1elPXxNyDorlGKN-1HzR7sPzJIHzNZAtes3FTixTM94s",
-            LocalBackupFile = $"{Application.dataPath}/BackupData/Town.csv",
-            MimeType = "text/csv"
+            LocalBackupFile = $"{Application.dataPath}/BackupData/Town.csv"
         };
 
         // Tutorial CSV
         public static readonly FileStorageData Tutorial = new FileStorageData()
         {
             GoogleDriveFileId = "1BB_AqHnOiwcTCAZ9qmOwC-Xm_faD2HiLYtrkgmK1LMA",
-            LocalBackupFile = $"{Application.dataPath}/BackupData/Tutorial.csv",
-            MimeType = "text/csv"
+            LocalBackupFile = $"{Application.dataPath}/BackupData/Tutorial.csv"
         };
 
         // Note: For testing purposes only - test CSV
-        public static readonly FileStorageData TestFile = new FileStorageData()
+        public static readonly FileStorageData TestCsv = new FileStorageData()
         {
             GoogleDriveFileId = "1i_W43TDJLPjL08V7knEnVfbeZonzHhXc5CED3fact8g",
-            LocalBackupFile = $"{Application.dataPath}/BackupData/TestFile.csv",
-            MimeType = "text/csv"
+            LocalBackupFile = $"{Application.dataPath}/BackupData/TestFile.csv"
+        };
+
+        // Test JSON - for testing only
+        public static readonly FileStorageData TestJson = new FileStorageData()
+        {
+            GoogleDriveFileId = "1yY_mL1qKoD1ROQCB1Zr3aH2UcxHNWrqg",
+            LocalBackupFile = $"{Application.dataPath}/BackupData/TestFile.json"
         };
 
         // Files list - used for creating backups.
@@ -71,7 +72,8 @@ namespace FileConstants
             Files.Items,
             Files.Town,
             Files.Tutorial,
-            Files.TestFile
+            Files.TestCsv,
+            Files.TestJson
         };
     }
 }
