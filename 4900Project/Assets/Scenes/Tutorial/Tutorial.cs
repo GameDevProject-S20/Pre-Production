7fa648f392da9f0b396f46c508517c3c20714731
Aaron Ramos-Lazette <aaronramoslazette@cmail.carleton.ca>
2020-07-08 04:05:10 -0400

    Fixed city specified in Tutorial dialogue

--- 4900Project/Assets/Scenes/Tutorial/Tutorial.cs
+++ 4900Project/Assets/Scenes/Tutorial/Tutorial.cs
@@ -21,7 +21,7 @@ public class Tutorial : MonoBehaviour
        enc = new Encounter(
             "Tutorial",
             "Tutorial",
-            "Welcome to town! I'm the Sheriff of these here parts! You'll be able to find the things you can do in my town on the Town Menu screen. Now, don't be going messing around in my town or you'll be hearing from me and your reputation will suffer. These are hard times and we can't be just letting anyone into our community, if your reputation drops low enough you'd best keep away. I see you're a traveling merchant, I hear the town of Riverbed is looking for some medicine. You can get that from the Pharmacy and bring it to them. Good luck!",
+            "Welcome to town! I'm the Sheriff of these here parts! You'll be able to find the things you can do in my town on the Town Menu screen. Now, don't be going messing around in my town or you'll be hearing from me and your reputation will suffer. These are hard times and we can't be just letting anyone into our community, if your reputation drops low enough you'd best keep away. I see you're a traveling merchant, I hear the town of York is looking for some medicine. You can get that from the Pharmacy and bring it to them. Good luck!",
             new string[]
             {
                             "Accept Quest"
