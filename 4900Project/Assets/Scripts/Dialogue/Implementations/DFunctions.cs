6819a93653de5793c5f02fdc11f549d61b3f13af
Sean Hodges <sean.hodges@nserc-crsng.gc.ca>
2020-07-11 11:32:11 -0400

    Quick bug fix

--- 4900Project/Assets/Scripts/Dialogue/Implementations/DFunctions.cs
+++ 4900Project/Assets/Scripts/Dialogue/Implementations/DFunctions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SIEvents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,7 +39,7 @@ namespace Dialogue
         {
             return () =>
             {
-                EventManager.Current.onDialogueSelect.Invoke(argument);
+                EventManager.Instance.onDialogueSelected.Invoke(argument);
             };
         }
     }
