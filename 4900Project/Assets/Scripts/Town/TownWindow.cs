c3ad09273de20115a167d22a27ac6b9bcf8d3c34
Keegan Koch <kkoch1397@gmail.com>
2020-07-13 02:30:48 -0400

    Town Window Framework created, need to update town data and find assets before fully integrating code for icons and the like but once ready just needs the proper datasetting to be ready.

---
+++ 4900Project/Assets/Scripts/Town/TownWindow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TownWindow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ActionPrefab;
+
+    Transform ActionMenu;
+    // stores the town data for later reference
+    private Town townData;
+
+    private void Start()
+    {
+        ActionMenu = transform.Find("TownBackground").Find("DataBorder").Find("DataBackground").Find("TownActionsBorder").Find("TownActionsBackground").Find("ActionList").Find("ActionGrid");
+        //townData = TownManager.Instance.GetCurrentTownData();
+        townData = new Town(15, "Big Test Town", "Sheriff Briggs");
+        transform.Find("TownBackground").Find("TownName").GetComponent<Text>().text = townData.Name;
+        transform.Find("TownBackground").Find("DataBorder").Find("DataBackground").Find("TownDataBackground").Find("TownData").Find("Description").GetComponent<Text>().text = "They are led by <color=" + townData.Colour + ">" + townData.Leader + "</color>.";
+
+        GameObject NewAction = Instantiate(ActionPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        NewAction.transform.parent = ActionMenu;
+        NewAction.transform.Find("Name").GetComponent<Text>().text = townData.Leader;
+        NewAction.transform.Find("Description").GetComponent<Text>().text = "I run this here town, talk to me if you need help.";
+
+        NewAction = Instantiate(ActionPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        NewAction.transform.Find("Name").GetComponent<Text>().text = townData.Leader;
+        NewAction.transform.parent = ActionMenu;
+        NewAction.transform.Find("Description").GetComponent<Text>().text = "I am Sheriff #2.";
+
+        NewAction = Instantiate(ActionPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        NewAction.transform.parent = ActionMenu;
+        NewAction.transform.Find("Name").GetComponent<Text>().text = townData.Leader;
+        NewAction.transform.Find("Description").GetComponent<Text>().text = "I'm Sheriff #3!";
+
+        NewAction = Instantiate(ActionPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        NewAction.transform.parent = ActionMenu;
+        NewAction.transform.Find("Name").GetComponent<Text>().text = townData.Leader;
+        NewAction.transform.Find("Description").GetComponent<Text>().text = "I'm the last Sheriff...";
+
+    }
+}
