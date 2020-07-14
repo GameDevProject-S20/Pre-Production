f097019269c4400e809ab112e142cab573b1965a
Cailyn Edwards <cailyn.edwards@shopify.com>
2020-07-14 14:26:19 -0400

    start script for navigation

---
+++ 4900Project/Assets/Scenes/HudNavMenu/HudActions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HudActions : MonoBehaviour
+{
+
+    public void OnInventoryButtonClick()
+        {
+            SceneManager.LoadScene("InventoryTestScene");
+            Debug.Log("Clicked");
+        }
+
+    //public void OnMenuButtonClick()
+    //{
+    //    TownMenu.SetActive(true);
+    //}
+}
