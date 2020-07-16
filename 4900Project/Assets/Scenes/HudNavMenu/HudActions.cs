43b94486ff54796480b444f41b16e38eae0ea769
Cailyn Edwards <cailyn.edwards@shopify.com>
2020-07-16 09:55:05 -0400

    add hud into most up to date main

---
+++ 4900Project/Assets/Scenes/HudNavMenu/HudActions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.ExitMenu;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HudActions : MonoBehaviour
+{
+
+    public void OnInventoryButtonClick()
+        {
+            SceneManager.LoadScene("InventoryTestScene", LoadSceneMode.Additive);
+            Debug.Log("Clicked");
+        }
+
+    public void OnMenuButtonClick()
+    {
+        ExitMenuControl.BringUpExitMenu();
+    }
+}
