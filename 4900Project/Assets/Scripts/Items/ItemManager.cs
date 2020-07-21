c685c16131ed7814afc03bc557a6894b1f77813d
Sean Hodges <sean.hodges@nserc-crsng.gc.ca>
2020-07-20 20:36:44 -0400

    Loading in items from a CSV

--- 4900Project/Assets/Scripts/Items/ItemManager.cs
+++ 4900Project/Assets/Scripts/Items/ItemManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using FileConstants;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,19 +13,19 @@ public enum typetag {Food,Luxury,Medicine,Building_Materials, Tools_And_Parts, C
 /// Intermediate class for handling loading items from a CSV file.
 /// This should be the class that gets generated, then each should be fed into the Item class.
 /// </summary>
+[Serializable]
 public struct ItemCsvData
 {
-    public string DisplayName;
-    public string Tooltip;
-    public string Description;
-    public float Value;
-    public float Weight;
-    public string Tier;
-    public string Tags;
-    public string IconName;
+    public string DisplayName { get; set; }
+    public string Tooltip { get; set; }
+    public string Description { get; set; }
+    public float Value { get; set; }
+    public float Weight { get; set; }
+    public string Tags { get; set; }
+    public string IconName { get; set; }
 }
 
-public struct Item 
+public class Item 
 {
     public string DisplayName;
     public string Tooltip;
@@ -33,6 +35,30 @@ public struct Item
     public rarity tier;
     public List<typetag> tags;
 
+    protected static rarity CalculateTier(float Value)
+    {
+        //create rarity based on value
+        if (Value >= 20)
+        {
+            return rarity.Unique;
+        }
+        else if (Value < 20 && Value >= 10)
+        {
+            return rarity.Rare;
+        }
+        else if (Value < 10 && Value >= 5)
+        {
+            return rarity.Uncommon;
+        }
+        else if (Value < 5 && Value >= 2)
+        {
+            return rarity.Common;
+        }
+        else
+        {
+            return rarity.Abundant;
+        }
+    }
     /// <summary>
     /// Creates an item given the values to set.
     /// This can be used for hardcoding items.
@@ -51,27 +77,7 @@ public struct Item
         Value = value_;
         Weight = weight_;
         tags = _tags;
-        //create rarity based on value
-        if (Value >= 20)
-        {
-            tier = rarity.Unique;
-        }
-        else if (Value < 20 && Value >= 10)
-        {
-            tier = rarity.Rare;
-        }
-        else if (Value < 10 && Value >= 5)
-        {
-            tier = rarity.Uncommon;
-        }
-        else if (Value < 5 && Value >= 2)
-        {
-            tier = rarity.Common;
-        }
-        else
-        {
-            tier = rarity.Abundant;
-        }
+        tier = CalculateTier(Value);
     }
 
     /// <summary>
@@ -86,8 +92,8 @@ public struct Item
         Description = itemData.Description;
         Value = itemData.Value;
         Weight = itemData.Weight;
-        tier = UnityHelperMethods.ParseEnum<rarity>(itemData.Tier);
         tags = UnityHelperMethods.ParseCommaSeparatedList<typetag>(itemData.Tags, UnityHelperMethods.ParseEnum<typetag>);
+        tier = CalculateTier(Value);
     }
 
 }
@@ -110,79 +116,12 @@ public class ItemManager : MonoBehaviour
         }
 
         //create item list
-
-        List<typetag> tempList = new List<typetag>();
-        tempList.Add(typetag.Food);
-        Item temp = new Item("Rations", "Unappetizing but cheap", "The bare minimum to keep someone running for a day", 1, 1, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Tools_And_Parts); //we will prob want a different tag for player essentials.
-        temp = new Item("Fuel", "A neccesity for travel between colonies", "Fuel is the lifeblood of traders, never leave port without first checking your tank. ", 1, 1, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Food);
-        tempList.Add(typetag.Luxury);
-        temp = new Item("Fresh Fruit", "Tasty, but spoils without proper storage", "A rarity in a good portion of the world, which makes is quite valuable to the right people. ", 5, 1, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Tools_And_Parts);
-        temp = new Item("Wrench", "An essential repair tool", "Helps with maintaining machines. Wear and tear means you need a replacement once in a while.", 2, 1, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Tools_And_Parts);
-        temp = new Item("Repair Patch", "One use smart repair patch", "Uncommon tech allows these patches to conduct repairs autonomously, but burn out performing the job", 15, 4, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Luxury);
-        temp = new Item("2000's Painting", "Collectors love this stuff", "Art from before the world ended. Nostalgic.", 19, 3, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Luxury);
-        temp = new Item("Jewelry", "A valuable, if nonfunctional, accessory", "Goes nice with a finely tailored outfit. Good luck finding one", 12, 1, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Building_Materials);
-        temp = new Item("Concrete", "Basic building material", "What most things are built out of these days", 3, 10, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Building_Materials);
-        tempList.Add(typetag.Tools_And_Parts);
-        temp = new Item("Scrap Metal", "General purpose parts", "Good for making something new or fixing something old", 5, 10, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Tools_And_Parts);
-        temp = new Item("Generator", "Creates electricity", "Consumes fuel to generate power. Vital for survival.", 40, 25, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Combat);
-        temp = new Item("Body Armor", "Keeps you intact", "Armor meant to protect from the typical bandit raid.", 4, 5, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Combat);
-        temp = new Item("RPG", "Sends a message", "Let anyone who intends to raid you serverly regret their choices for a whole second.", 20, 2, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Medicine);
-        temp = new Item("Medicine", "Patches up wounds.", "General pruopse bandages and medicine to treat injuries and illnesses", 6, 3, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
-
-        tempList = new List<typetag>();
-        tempList.Add(typetag.Medicine);
-        tempList.Add(typetag.Luxury);
-        temp = new Item("Medical Kit", "High end First Aid Kit for dangerous work", "NOt only stocked with the best medical supplies on hand, but even contain stimulants and boosters to avoid injury in the first place", 20, 2, tempList);
-        itemsMaster.Add(temp.DisplayName, temp);
+        GameData.LoadCsv(Files.Items, out IEnumerable<ItemCsvData> itemsData);
+        foreach (var itemData in itemsData)
+        {
+            var item = new Item(itemData);
+            itemsMaster.Add(item.DisplayName, item);
+        }
     }
 
     Dictionary<string, Item> GetListofType(List<typetag> types)
