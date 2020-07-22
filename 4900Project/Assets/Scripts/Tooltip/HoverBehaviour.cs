bd89708a14632f1055b86ba3a4fa5740d04d6ee6
Keegan <kkoch1397@gmail.com>
2020-07-22 04:16:06 -0400

    Inventory ToolTip

---
+++ 4900Project/Assets/Scripts/Tooltip/HoverBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class HoverBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Tooltip tooltip;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tooltip = GameObject.Find("Tooltip").GetComponent<Tooltip>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        string name = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text;
+        int index = name.IndexOf("(");
+        if(index > 0)
+        {
+            name = name.Substring(0, index-1);
+        }
+        Debug.Log(name);
+        Item temp;
+        ItemManager.Current.itemsMaster.TryGetValue(name, out temp);
+        tooltip.GenerateTooltip(temp);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.gameObject.SetActive(false);
+    }
+}
---
+++ 4900Project/Assets/Scripts/Tooltip/Tooltip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour
+{
+    private Component[] texts;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gameObject.GetComponent<Image>().enabled = false;
+        texts = GetComponentsInChildren<Text>();
+        foreach (Text tex in texts)
+        {
+            tex.gameObject.SetActive(false);
+        }
+    }
+
+    public void GenerateTooltip (Item item)
+    {
+        gameObject.GetComponent<Image>().enabled = true;
+        gameObject.SetActive(true);
+        texts = GetComponentsInChildren<Text>(true);
+        foreach (Text tex in texts)
+        {
+            tex.gameObject.SetActive(true);
+        }
+
+        gameObject.transform.GetChild(0).GetComponent<Text>().text = item.DisplayName;
+        gameObject.transform.GetChild(1).GetComponent<Text>().text = item.Tooltip;
+    }
+}
