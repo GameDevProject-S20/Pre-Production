37dfe4d4e485da49bbe67d6e7a0a0eda522396ab
fredghostkyle <fredghostkyle@yahoo.com>
2020-07-22 21:48:19 -0400

    Map is now dragable

--- 4900Project/Assets/Scripts/OverworldMap/MapDisplay/SimpleCameraController.cs
+++ 4900Project/Assets/Scripts/OverworldMap/MapDisplay/SimpleCameraController.cs
@@ -1,30 +1,49 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleCameraController : MonoBehaviour
 {
-    [SerializeField]
+    //[SerializeField] Currently not used // WSAD Controls 
     float panSpeed = 0.1f;
-    [SerializeField]
-    float zoomSpeed = 4.0f;
-    [SerializeField]
-    Vector3 max;
-    [SerializeField]
-    Vector3 min;
+    Vector3 min; //4.68, 10, 6.51
+    Vector3 max; //-4.68, 4, -6.54
 
+
+    [Header("Controls for zoom with scroll wheel")]
+    [SerializeField]
+    float zoomSpeed; // 4
+    [SerializeField]
+    float scrollMax; // 10
+    [SerializeField]
+    float scrollMin; // 4
+
+    [Header("Settings for mouse dragging")]
+    [SerializeField]
+    public float dragSpeed; // -0.25
+    [SerializeField]
+    public float outerLeft; // -10
+    [SerializeField]
+    public float outerRight; // 10
+    [SerializeField]
+    public float outerUp; // 12
+    [SerializeField]
+    public float outerDown; // -12 
+
+    private Vector3 dragOrigin;
+
+    // Currently not used 
     float panBorderSizeInMenu = 15;
     float panBorderSize = 80;
-
-    private Vector3 lastMousePosition = Vector3.zero;
-    private bool mouseJustClicked = false;
 
 
     private void Update() 
     {
-        transform.position = GeneralPurposeControl(transform.position);
-        transform.position = MousePanControl(transform.position);
-        transform.position = MouseScrollControl(transform.position);
+        //transform.position = GeneralPurposeControl(transform.position); //WSAD! 
+        transform.position = MouseScrollControl(transform.position); ;
+        MouseDragControl();
+        
     }
 
 
@@ -83,8 +102,8 @@ public class SimpleCameraController : MonoBehaviour
         velocity *= panSpeed;
 
         Vector3 nextPosition = position + velocity;
-        nextPosition.x = Mathf.Clamp(nextPosition.x, min.x, max.x);
-        nextPosition.z = Mathf.Clamp(nextPosition.z, min.z, max.z);
+        nextPosition.x = Mathf.Clamp(nextPosition.x, scrollMin, scrollMax);
+        nextPosition.z = Mathf.Clamp(nextPosition.z, scrollMin, scrollMax);
 
         return nextPosition;
     }
@@ -94,31 +113,41 @@ public class SimpleCameraController : MonoBehaviour
         Vector3 nextPosition = position;
 
         nextPosition.y -= Input.mouseScrollDelta.y * zoomSpeed;
-        nextPosition.y = Mathf.Clamp(nextPosition.y, min.y, max.y);
+        nextPosition.y = Mathf.Clamp(nextPosition.y, scrollMin, scrollMax);
 
         return nextPosition;
     }
 
-    public Vector3 MouseDragControl(Vector3 position)
+    public void MouseDragControl()
     {
-        Vector3 nextPosition = position;
-        Vector3 input = Vector3.zero;
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, position.y));
 
-        if (Input.GetMouseButton(0) && !mouseJustClicked)
+        if (Input.GetMouseButtonDown(0))
         {
-            mouseJustClicked = true;
-            lastMousePosition = mousePosition;
+            dragOrigin = Input.mousePosition;
+            return;
         }
-        if (Input.GetMouseButton(0)) {
-            input = (lastMousePosition - mousePosition);
-            lastMousePosition = mousePosition;
 
-            nextPosition = position + input;
+        if (!Input.GetMouseButton(0)) return;
+
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+
+        // Calculate the expect translation to make sure it does not exceed max/min 
+        Vector3 expectedTranslation = transform.position + move;
+
+
+        /* Check t0 make sure not outside of the bounds */ 
+        if (outerRight > expectedTranslation.x && expectedTranslation.x > outerLeft)
+        {
+            transform.Translate(new Vector3(move.x, 0, 0), Space.World);
         }
-          else {
-            mouseJustClicked = false;
+
+        if (outerUp > expectedTranslation.z && expectedTranslation.z > outerDown)
+        {
+            transform.Translate(new Vector3(0, 0, move.z), Space.World);
         }
-        return nextPosition;
+
     }
 }
