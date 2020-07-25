95524fe62a88b3ebabad5e1bd79487bc76518626
Aaron Ramos-Lazette <aaronramoslazette@cmail.carleton.ca>
2020-07-25 06:44:13 -0400

    Added Radomizing Effect

---
+++ 4900Project/Assets/Scripts/Events/Effects/RandomEffect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+public class RandomEffect : IEffect
+{
+    private static Random random = new System.Random();
+    private IEffect effect;
+
+    public RandomEffect(IEffect first, IEffect second, double percentFirst)
+    {
+        if (percentFirst < 0.0f || percentFirst > 1.0f)
+        {
+            throw new ArgumentException("percentFirst must be between 0 and 1, inclusive");
+        }
+
+        effect = (random.NextDouble() <= percentFirst) ? first : second;
+
+        UnityEngine.Debug.Log("RandomEffect: " + effect);
+    }
+
+    public bool Apply()
+    {
+        return effect.Apply();
+    }
+}
