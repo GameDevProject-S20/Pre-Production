067c3f54a936e3ccb4f09b7cd358689eaad3ac7c
Sean Hodges <sean.hodges@nserc-crsng.gc.ca>
2020-08-01 20:37:28 -0400

    Add some commenting into the EscapeMenuControl class

--- 4900Project/Assets/Scripts/EscapeMenu/Frontend/EscapeMenuControl.cs
+++ 4900Project/Assets/Scripts/EscapeMenu/Frontend/EscapeMenuControl.cs
@@ -11,10 +11,26 @@ namespace Assets.Scripts.EscapeMenu.Interfaces
 {
     class EscapeMenuControl : MonoBehaviour
     {
+        // Public Properties
+        /// <summary>
+        /// Button for quitting the game
+        /// </summary>
         public Button quitButton;
+
+        /// <summary>
+        /// Button for closing the Escape Menu
+        /// </summary>
         public Button closeButton;
+        
+        /// <summary>
+        /// Every Slider that the Escape Menu should be controlling, along with additional data
+        /// </summary>
         public List<SettingsSliderData> sliders;
 
+        // Public Methods
+        /// <summary>
+        /// Startup - Hook everything up
+        /// </summary>
         void Start()
         {
             // Hook up the quit button
@@ -30,11 +46,20 @@ namespace Assets.Scripts.EscapeMenu.Interfaces
 
         }
 
+        /// <summary>
+        /// Hide the escape menu canvas
+        /// </summary>
         public void Hide()
         {
             gameObject.GetComponent<Canvas>().enabled = false;
         }
 
+        // Private Methods
+        /// <summary>
+        /// Hooks up a single slider. Should only be called on start up, but keeping this separate as a separation of concerns.
+        /// Given the SettingsSliderData for the slider, hook it in to the UI & backend.
+        /// </summary>
+        /// <param name="sliderData"></param>
         private void HookupSlider(SettingsSliderData sliderData)
         {
             var slider = sliderData.slider;
