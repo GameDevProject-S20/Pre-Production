02c00c8f7fd6a3bfe7b59a33b268c7908d5bf3fb
Keegan <kkoch1397@gmail.com>
2020-08-06 03:54:04 -0400

    adding Hud warnings + weight tracker

--- 4900Project/Assets/Scenes/HudNavMenu/StatusBars.cs
+++ 4900Project/Assets/Scenes/HudNavMenu/StatusBars.cs
@@ -17,16 +17,32 @@ public class StatusBars : MonoBehaviour
     [SerializeField]
     private Image fuelBar;
 
+    [SerializeField]
+    private Text weightBarText;
+    [SerializeField]
+    private Image weightBar;
+
+    [SerializeField]
+    private Text HealthWarning;
+    [SerializeField]
+    private Text FuelWarning;
+
     void Awake()
     {
         UpdateAmounts();
         EventManager.Instance.OnHealthChange.AddListener(UpdateAmounts);
+        InvokeRepeating("WarningCheck", 0.75f, 0.75f);
+        HealthWarning.enabled = false;
+        FuelWarning.enabled = false;
     }
 
     private void Update() {
         int count = DataTracker.Current.Player.Inventory.Contains("Fuel");
         fuelBarText.text = count.ToString("0");
         fuelBar.fillAmount = count / 20.0f;
+        float temp = DataTracker.Current.Player.Inventory.GetWeightRatio();
+        weightBarText.text = (temp*100).ToString() + "% Capacity";
+        weightBar.fillAmount = temp;
     }
 
     void UpdateAmounts(int i=0)
@@ -35,5 +51,31 @@ public class StatusBars : MonoBehaviour
         healthBar.fillAmount = DataTracker.Current.Player.Health / (float)DataTracker.Current.Player.HealthCap;
 
 
+    }
+
+    void WarningCheck()
+    {
+        int count = DataTracker.Current.Player.Inventory.Contains("Fuel");
+        if(count < 12)
+        {
+            fuelBar.enabled = !fuelBar.enabled;
+            FuelWarning.enabled = true;
+        }
+        else
+        {
+            fuelBar.enabled = true;
+            FuelWarning.enabled = false;
+        }
+        count = DataTracker.Current.Player.Health;
+        if (count < 30)
+        {
+            healthBar.enabled = !healthBar.enabled;
+            HealthWarning.enabled = true;
+        }
+        else
+        {
+            healthBar.enabled = true;
+            HealthWarning.enabled = false;
+        }
     }
 }
