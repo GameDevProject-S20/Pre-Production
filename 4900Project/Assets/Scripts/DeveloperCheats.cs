9b903f0eee665995abd5cd4ba4bb05d9b7f0444f
Wilson Hodgson <wilsonhodgsonhodgson@cmail.carleton.ca>
2020-08-08 13:59:43 -0400

    Adds Developer mode button if DeveloperCheats is enabled on DataTracker

--- 4900Project/Assets/Scripts/DeveloperCheats.cs
+++ 4900Project/Assets/Scripts/DeveloperCheats.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class DeveloperCheats : MonoBehaviour
 {
     // Start is called before the first frame update
+
     void Start()
+    {
+        // Load Dev Mode Button
+        var DevModeButton = Resources.Load("Prefabs/General/DeveloperModeButton");
+        GameObject devmode = (GameObject)GameObject.Instantiate(DevModeButton);
+
+        // Add Dev Mode Functionality
+        devmode.GetComponentInChildren<UnityEngine.UI.Button>().onClick.AddListener(devModeEnable);
+        // Add Button Functionality
+        devmode.GetComponentInChildren<UnityEngine.UI.Button>().onClick.AddListener(() => {GameObject.Find("Initializer").GetComponent<Initializer>().OnEnterGameClick();});
+    }
+
+    public void devModeEnable()
     {
         //Add Edge for Skipping Tutorial
         DataTracker.Current.WorldMap.AddEdge(119, 124);
-        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void devModeDisable(){
+        DataTracker.Current.WorldMap.RemoveEdge(119, 124);
     }
 }
