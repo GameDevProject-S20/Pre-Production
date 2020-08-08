10e2eab76b413d8c02dcb01eae7c7931c9e33b4d
Wilson Hodgson <wilsonhodgsonhodgson@cmail.carleton.ca>
2020-08-08 12:57:28 -0400

    Removes the Edge between Tutorial and World, but adds a DeveloperCheats script to the Datatracker scene which is disabled by default

---
+++ 4900Project/Assets/Scripts/DeveloperCheats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeveloperCheats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Add Edge for Skipping Tutorial
+        DataTracker.Current.WorldMap.AddEdge(119, 124);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
