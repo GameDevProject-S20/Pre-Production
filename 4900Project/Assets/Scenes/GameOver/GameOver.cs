be576eb582d7a2663db1737109f2b8bd74479f27
Cailyn Edwards <cailyn.edwards@shopify.com>
2020-08-11 11:11:20 -0400

    create gameover scene

---
+++ 4900Project/Assets/Scenes/GameOver/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using Dialogue;
+using Encounters;
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public RawImage Background;
+
+    private void Start()
+    {
+        
+    }
+
+}
