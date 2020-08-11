0157d15b6541140d77f2b3a9a6ac66e57b69f473
fredghostkyle <fredghostkyle@yahoo.com>
2020-08-11 13:55:28 -0400

    Added GameOverController script and linked buttons

--- 4900Project/Assets/Scenes/GameOver/GameOverUI.cs
+++ 4900Project/Assets/Scenes/GameOver/GameOverUI.cs
@@ -7,13 +7,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameOver : MonoBehaviour
+public class GameOverUI : MonoBehaviour
 {
     public RawImage Background;
+
 
     private void Start()
     {
         
+    }
+
+    public void OnTryAgainClick()
+    {
+        SceneManager.LoadScene("InitializerScene", LoadSceneMode.Single);
+    }
+
+    public void OnMainMenuClick()
+    {
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
     }
 
 }
